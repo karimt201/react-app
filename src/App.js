5cdf9974ba079407c93d8364daf2b6ec96439ab5
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-08-02 23:01:38 +0300

    set first function in react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function app() {
+    return (
+        <div>
+            <h1>first</h1>
+            <h2>course</h2>
+            <h3>in React in this</h3>
+        </div>
+    )
+};
+
+export default app ;
