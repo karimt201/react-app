a5b42ca9c39c2d401f1698b26a9a440b956c098f
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-08-11 19:32:36 +0300

    new project , add logo

---
+++ src/Components/navbar.js
@@ -0,0 +1,7 @@
+export default function Nav(){
+    return(
+        <img src={require("../cov.jpg")} style = {{
+            width : '200px'
+        }}alt="Logo"  />
+    )
+};
--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css'
 import App from './App'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-
+import Navbar from './Components/navbar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <div>
     <App />
-    <Header />
-    <Footer />
+    <Navbar />
 </div>);
