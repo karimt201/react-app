b5a72550bc4c2cf5d6889d0ca09b41dadafefcc6
karim tarek <142750275+karimt201@users.noreply.github.com>
2024-08-12 15:33:39 +0300

    second project

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import Cards from "./Components/Cards"
 import { data } from "./data"
 
 export default function App(){
-    const datashow = data.map((cs) => <Cards 
-    img = {cs.img}
-    title = {cs.title}
-    desc = {cs.desc}
-    rate = {cs.rate}
-    price = {cs.Price}
+    const datashow = data.map((cs , index) =>
+    <Cards 
+        img = {cs.img}
+        title = {cs.title}
+        desc = {cs.desc}
+        rate = {cs.rate}
+        price = {cs.Price}
     
     />)
     return (
-        <div>
+        <div style={{
+            display : "flex",
+            alignItems : "center",
+            justifyContent : "center",
+            gap : '20px',
+            flexWrap : 'wrap',
+            }}>
             {datashow}
         </div>
     )
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,15 +1,24 @@
 export default function Cards(props){
-    return <div style={{width : '400px'}}>
-        <img src={props.img} style={{width : '400px'}} alt = 'city' />
+    return <div style={{ }}>
+        <div style={{
+            backgroundImage : `url(${props.img})`,
+            width : '300px',
+            height : '300px',
+            backgroundRepeat : 'no-repeat',
+            backgroundSize : 'cover',
+            backgroundPosition : 'center',
+            borderRadius : '12px',
+        }}></div>
         <div style={{
             display : 'flex',
             alignItems : 'center',
-            justifyContent : 'space-between'
+            justifyContent : 'space-between',
+            marginBottom : '0px',
         }}>
-        <h1 style={{marginBottom : '0px'}}> {props.title}</h1>
-        <h3>{props.rate}</h3>
+        <h1 style={{}}> {props.title}</h1>
+        <h3 style={{ fontWeight : 'normal'}}>{props.rate}</h3>
         </div>
         <h3 style={{color : 'gray', marginTop : '0px'}}>{props.desc}</h3>
-        <h3>{props.price} night</h3>
+        <h3>{props.price} <span style={{fontWeight : 'normal'}}>night</span></h3>
     </div>
 }
